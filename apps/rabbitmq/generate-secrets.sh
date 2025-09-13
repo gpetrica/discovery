bf43ac432ce2021dd0360a2c75b547a3649efddc
Petrica Ghiurca <petrica.ghiurca@zuppler.com>
2025-09-13 20:36:46 +0200

    rabbitmq secrets

--- apps/rabbitmq/generate-secrets.sh
+++ apps/rabbitmq/generate-secrets.sh
@@ -2,12 +2,12 @@
 
 kubectl create secret generic rabbitmq-auth \
 	--from-env-file=.env \
-	-n pos \
+	-n rabbitmq \
 	--dry-run=client -o yaml >rabbitmq-auth.yaml
 
 kubeseal \
 	--controller-name=sealed-secrets-controller \
 	--controller-namespace=kube-system \
-	-o yaml <rabbitmq-auth.yaml >secrets/rabbitmq-auth-sealed.yaml
+	-o yaml <rabbitmq-auth.yaml >rabbitmq-auth-sealed.yaml
 
 rm rabbitmq-auth.yaml
